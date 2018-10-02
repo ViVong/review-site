af4c2c15209e59dfcbd25b9bb56698bd5513e9e6
Vi Vong <veixrath@gmail.com>
2018-10-02 13:02:53 -0400

    Added review class and application

--- src/main/java/org/wecancodeit/review/ReviewController.java
+++ src/main/java/org/wecancodeit/review/ReviewController.java
@@ -11,5 +11,5 @@ public class ReviewController {
 		return "home";
 	}
 	
-	
+	//@RequestMapping()
 }
--- src/main/java/org/wecancodeit/review/ReviewRepository.java
+++ src/main/java/org/wecancodeit/review/ReviewRepository.java
@@ -5,4 +5,16 @@ import java.util.HashMap;
 public class ReviewRepository {
 	HashMap<Long, Review> review = new HashMap<Long, Review>();
 	
+	public void addReview( long id, int month, int date, String title, String url, String category, String content) {
+		Review newReview = new Review(id, month, date, title, url, category, content);
+		review.put(newReview.GetID(), newReview);
+	}
+	
+	public Review oneReview(long id) {
+		return review.get(id);
+	}
+	
+	public HashMap<Long, Review> allReviews() {
+		return review;
+	}
 }

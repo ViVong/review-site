6c713c7b339bcad1005cbabab8db29f94dde426c
Vi Vong <veixrath@gmail.com>
2018-10-02 16:26:41 -0400

    Added request mappings and templates

--- src/main/java/org/wecancodeit/review/ReviewController.java
+++ src/main/java/org/wecancodeit/review/ReviewController.java
@@ -1,15 +1,28 @@
 package org.wecancodeit.review;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReviewController {
 	
+	@Resource
+	ReviewRepository reviewRepo;
+	
 	@RequestMapping("/")
-	public String homePage() {
+	public String homePage(Model model) {
+		model.addAttribute("reviews", reviewRepo.allReviews());
 		return "home";
 	}
 	
-	//@RequestMapping()
+	@RequestMapping("/review")
+	public String showOneReview(@RequestParam(value="id") Long id, Model model) {
+		model.addAttribute("reviews", reviewRepo.oneReview(id));
+		return "review";
+	}
+	
 }
--- src/main/java/org/wecancodeit/review/ReviewRepository.java
+++ src/main/java/org/wecancodeit/review/ReviewRepository.java
@@ -9,6 +9,13 @@ import org.springframework.stereotype.Repository;
 public class ReviewRepository {
 	private HashMap<Long, Review> review = new HashMap<Long, Review>();
 	
+	public ReviewRepository() {
+		Review review1 = new Review( 012345L, 8, 20, "A review on reviews.", "/review", "General", "Reviews are lame and I don't like them");
+		Review review2 = new Review( 000777L, 7, 07, "Top 10 Irish products", "/irish", "General", "10. Twisted shammrocks\n9. Electric Bagpipes");
+		review.put(review1.GetID(), review1);
+		review.put(review2.GetID(), review1);
+	}
+	
 	public ReviewRepository(Review...newReview) {
 		for (Review reviews: newReview) {
 			review.put(reviews.GetID(), reviews);
